bee6afeb51cc4687649ab9ccc12ca65a03009add
Melvin <melvingit@gmail.com>
2023-05-11 15:18:18 -0700

    reverted cannon to older version

--- week13/cannon.py
+++ week13/cannon.py
@@ -5,9 +5,9 @@ from random import randint, gauss
 pg.init()
 pg.font.init()
 
-WHITE = (260, 260, 260)
+WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (260, 0, 0)
+RED = (255, 0, 0)
 
 SCREEN_SIZE = (800, 600)
 
